ff216c48a7a1c7af0d25ae97ce59c805cb74a1ed
Gorgi Kostov <39196442+furiouskopan@users.noreply.github.com>
2024-07-29 23:01:15 +0200

    update

--- routemapping.client/src/app/map/map.component.ts
+++ routemapping.client/src/app/map/map.component.ts
@@ -1,6 +1,9 @@
-// src/app/map/map.component.ts
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, latLng } from 'leaflet';
+import { tileLayer, latLng, polyline, Map, LeafletMouseEvent, marker } from 'leaflet';
+import { RouteService, Route, RoutePoint } from '../route.service';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-map',
@@ -9,6 +12,12 @@ import { tileLayer, latLng } from 'leaflet';
 })
 export class MapComponent implements OnInit {
   options: any;
+  map: Map | undefined;
+  isCreatingRoute = false;
+  routePoints: RoutePoint[] = [];
+  currentPolyline: any;
+
+  constructor(private routeService: RouteService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.options = {
@@ -18,5 +27,80 @@ export class MapComponent implements OnInit {
       zoom: 13,
       center: latLng(41.437, 22.641) // Centered on Strumica
     };
+    this.loadRoadData();
+  }
+
+  onMapReady(map: Map): void {
+    this.map = map;
+    (L as any).Routing.control({
+      waypoints: [],
+      routeWhileDragging: true
+    }).addTo(this.map);
+  }
+
+  loadRoadData(): void {
+    const overpassUrl = 'https://overpass-api.de/api/interpreter?data=[out:json];way["highway"](around:50000,41.437,22.641);out;>;out skel qt;';
+    this.http.get(overpassUrl).subscribe((data: any) => {
+      this.addRoadDataToMap(data);
+    });
+  }
+
+  addRoadDataToMap(data: any): void {
+    const roads = data.elements.filter((element: any) => element.type === 'way');
+    roads.forEach((road: any) => {
+      const points = road.nodes.map((node: any) => latLng(node.lat, node.lon));
+      const isOneWay = road.tags.oneway === 'yes';
+      L.polyline(points, { color: isOneWay ? 'red' : 'blue' }).addTo(this.map!);
+    });
+  }
+
+  loadRoutes(): void {
+    this.routeService.getRoutes().subscribe((routes: Route[]) => {
+      routes.forEach(route => {
+        const points = route.routePoints.map((point: RoutePoint) => latLng(point.latitude, point.longitude));
+        polyline(points).addTo(this.map!);
+      });
+    });
+  }
+
+  startRoute(): void {
+    this.isCreatingRoute = true;
+    this.routePoints = [];
+    if (this.currentPolyline) {
+      this.map?.removeLayer(this.currentPolyline);
+    }
+  }
+
+  onMapClick(event: LeafletMouseEvent): void {
+    if (this.isCreatingRoute) {
+      const point: RoutePoint = {
+        routePointId: 0,
+        latitude: event.latlng.lat,
+        longitude: event.latlng.lng,
+        order: this.routePoints.length + 1,
+        routeId: 0,
+      };
+      this.routePoints.push(point);
+      marker([point.latitude, point.longitude]).addTo(this.map!);
+
+      if (this.currentPolyline) {
+        this.map?.removeLayer(this.currentPolyline);
+      }
+
+      const points = this.routePoints.map(p => latLng(p.latitude, p.longitude));
+      this.currentPolyline = polyline(points).addTo(this.map!);
+    }
+  }
+
+  saveRoute(): void {
+    const route: Route = {
+      routeId: 0,
+      name: `Route ${new Date().toLocaleString()}`,
+      routePoints: this.routePoints
+    };
+    this.routeService.createRoute(route).subscribe(() => {
+      this.isCreatingRoute = false;
+      this.loadRoutes();
+    });
   }
 }
--- routemapping.client/src/app/route.service.ts
+++ routemapping.client/src/app/route.service.ts
@@ -1,13 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Route } from './models/route';
+
+export interface Route {
+  routeId: number;
+  name: string;
+  routePoints: RoutePoint[];
+}
+
+export interface RoutePoint {
+  routePointId: number;
+  latitude: number;
+  longitude: number;
+  order: number;
+  routeId: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class RouteService {
-  private apiUrl = 'http://localhost:5000/api/route';
+  private apiUrl = 'https://localhost:5001/api/routes';
 
   constructor(private http: HttpClient) { }
 
@@ -15,19 +28,7 @@ export class RouteService {
     return this.http.get<Route[]>(this.apiUrl);
   }
 
-  getRoute(id: number): Observable<Route> {
-    return this.http.get<Route>(`${this.apiUrl}/${id}`);
-  }
-
-  saveRoute(route: Route): Observable<Route> {
+  createRoute(route: Route): Observable<Route> {
     return this.http.post<Route>(this.apiUrl, route);
-  }
-
-  updateRoute(id: number, route: Route): Observable<void> {
-    return this.http.put<void>(`${this.apiUrl}/${id}`, route);
-  }
-
-  deleteRoute(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
