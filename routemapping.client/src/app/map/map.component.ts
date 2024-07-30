8febff644e7d5df411408bb8e21597f1a3bd6498
Gorgi Kostov <39196442+furiouskopan@users.noreply.github.com>
2024-07-30 16:42:49 +0200

    Map working with highlighting roads, need to remove directions and fix the order of the markers

--- routemapping.client/src/app/map/map.component.ts
+++ routemapping.client/src/app/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, latLng, polyline, Map, LeafletMouseEvent, marker } from 'leaflet';
+import { tileLayer, latLng, polyline, Map, LeafletMouseEvent, marker, Marker } from 'leaflet';
 import { RouteService, Route, RoutePoint } from '../route.service';
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
@@ -16,6 +16,9 @@ export class MapComponent implements OnInit {
   isCreatingRoute = false;
   routePoints: RoutePoint[] = [];
   currentPolyline: any;
+  markers: Marker[] = [];
+  routeDistance: number = 0;
+  routeDuration: number = 0;
 
   constructor(private routeService: RouteService, private http: HttpClient) { }
 
@@ -66,6 +69,7 @@ export class MapComponent implements OnInit {
   startRoute(): void {
     this.isCreatingRoute = true;
     this.routePoints = [];
+    this.clearMarkers();
     if (this.currentPolyline) {
       this.map?.removeLayer(this.currentPolyline);
     }
@@ -81,7 +85,11 @@ export class MapComponent implements OnInit {
         routeId: 0,
       };
       this.routePoints.push(point);
-      marker([point.latitude, point.longitude]).addTo(this.map!);
+      const newMarker: Marker = marker([point.latitude, point.longitude], { draggable: true })
+        .on('dragend', (e) => this.updateRoutePoints(e))
+        .on('contextmenu', () => this.removePoint(newMarker))
+        .addTo(this.map!);
+      this.markers.push(newMarker);
 
       if (this.currentPolyline) {
         this.map?.removeLayer(this.currentPolyline);
@@ -89,7 +97,29 @@ export class MapComponent implements OnInit {
 
       const points = this.routePoints.map(p => latLng(p.latitude, p.longitude));
       this.currentPolyline = polyline(points).addTo(this.map!);
+      this.calculateRouteDetails();
+    } else {
+      // Highlight the road to the clicked location
+      this.highlightRoad(event.latlng);
     }
+  }
+
+  updateRoutePoints(event: any): void {
+    const movedMarker = event.target;
+    const newLatLng = movedMarker.getLatLng();
+    const index = this.markers.indexOf(movedMarker);
+    if (index !== -1) {
+      this.routePoints[index].latitude = newLatLng.lat;
+      this.routePoints[index].longitude = newLatLng.lng;
+    }
+
+    if (this.currentPolyline) {
+      this.map?.removeLayer(this.currentPolyline);
+    }
+
+    const points = this.routePoints.map(p => latLng(p.latitude, p.longitude));
+    this.currentPolyline = polyline(points).addTo(this.map!);
+    this.calculateRouteDetails();
   }
 
   saveRoute(): void {
@@ -102,5 +132,85 @@ export class MapComponent implements OnInit {
       this.isCreatingRoute = false;
       this.loadRoutes();
     });
+  }
+
+  clearMarkers(): void {
+    this.markers.forEach(marker => this.map?.removeLayer(marker));
+    this.markers = [];
+  }
+
+  removePoint(marker: Marker): void {
+    const index = this.markers.indexOf(marker);
+    if (index !== -1) {
+      this.routePoints.splice(index, 1);
+      this.map?.removeLayer(marker);
+      this.markers.splice(index, 1);
+
+      if (this.currentPolyline) {
+        this.map?.removeLayer(this.currentPolyline);
+      }
+
+      const points = this.routePoints.map(p => latLng(p.latitude, p.longitude));
+      this.currentPolyline = polyline(points).addTo(this.map!);
+      this.calculateRouteDetails();
+    }
+  }
+
+  clearAllRoutes(): void {
+    this.routePoints = [];
+    if (this.currentPolyline) {
+      this.map?.removeLayer(this.currentPolyline);
+    }
+    this.clearMarkers();
+  }
+
+  calculateRouteDetails(): void {
+    if (this.routePoints.length > 1) {
+      let distance = 0;
+      for (let i = 0; i < this.routePoints.length - 1; i++) {
+        const point1 = this.routePoints[i];
+        const point2 = this.routePoints[i + 1];
+        distance += this.getDistance(latLng(point1.latitude, point1.longitude), latLng(point2.latitude, point2.longitude));
+      }
+      this.routeDistance = distance;
+      this.routeDuration = (distance / 50) * 60; // Assuming an average speed of 50 km/h
+    } else {
+      this.routeDistance = 0;
+      this.routeDuration = 0;
+    }
+  }
+
+  getDistance(point1: L.LatLng, point2: L.LatLng): number {
+    const R = 6371; // Radius of the Earth in km
+    const dLat = this.toRad(point2.lat - point1.lat);
+    const dLon = this.toRad(point2.lng - point1.lng);
+    const lat1 = this.toRad(point1.lat);
+    const lat2 = this.toRad(point2.lat);
+
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const distance = R * c;
+    return distance;
+  }
+
+  toRad(value: number): number {
+    return value * Math.PI / 180;
+  }
+
+  highlightRoad(destination: L.LatLng): void {
+    if (this.map) {
+      (L as any).Routing.control({
+        waypoints: [latLng(41.437, 22.641), destination], // Starting point is fixed, update if needed
+        routeWhileDragging: true
+      }).addTo(this.map);
+    }
+  }
+
+  exportRouteToGoogleMaps(): void {
+    const baseUrl = 'https://www.google.com/maps/dir/?api=1&travelmode=driving&waypoints=';
+    const waypoints = this.routePoints.map(point => `${point.latitude},${point.longitude}`).join('|');
+    const url = `${baseUrl}${waypoints}`;
+    window.open(url, '_blank');
   }
 }
